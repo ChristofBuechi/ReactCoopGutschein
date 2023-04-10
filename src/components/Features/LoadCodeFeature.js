9c71e5b3c1074155f0e582a1f22ae1fbbf4876e0
Christof <christof.buechi@gmail.com>
2023-04-10 11:12:23 +0200

    add Barcode

--- src/components/Features/LoadCodeFeature.js
+++ src/components/Features/LoadCodeFeature.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import classes from "./LoadCodeFeature.module.css";
 import Card from "../UI/Card";
 import QRCode from "react-qr-code";
+import Barcode from "react-barcode";
 
 function LoadCodeFeature() {
     const url = "https://coopcouponcloudfunction-x2hqhf3efq-oa.a.run.app/";
@@ -20,7 +21,7 @@ function LoadCodeFeature() {
         const fetchData = async () => {
             const response = await fetch(url);
             const json = await response.json();
-            console.log(JSON.stringify(`loaded: ${JSON.stringify(json)}`));
+            console.log(`loaded: ${JSON.stringify(json)}`);
             setData(json);
             setIsLoading(false);
         };
@@ -37,9 +38,13 @@ function LoadCodeFeature() {
     // Render QR code component with fetched code value
     return <Card className={classes.form}>
         <h2>Loaded QR Code:</h2>
+        <div style={{ background: 'white', padding: '25px' }}>
             {<QRCode value={data.code}/>}
+        </div>
         <pre>{`Rabatt ${data.value}`}</pre>
-
+        <div style={{ background: 'white', padding: '16px' }}>
+            <Barcode value={data.code} />
+        </div>
         </Card>;
 }
 
